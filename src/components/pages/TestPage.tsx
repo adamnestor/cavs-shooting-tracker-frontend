1287a0459b572206d78e5b9d12101ed51babe46a
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2025-11-14 16:09:30 -0500

    Add buttons to TestPage with state handling

--- src/components/pages/TestPage.tsx
+++ src/components/pages/TestPage.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import { Button } from "../common/Button";
+
 export function TestPage() {
-  return <div>This is the test page!</div>;
+  const [AttemptedCount, SetAttemptCount] = useState(0);
+  const [MadeCount, SetMadeCount] = useState(0);
+
+  function handleMade() {
+    SetAttemptCount((prev) => prev + 1);
+    SetMadeCount((prev) => prev + 1);
+  }
+
+  function handleMiss() {
+    SetAttemptCount((prev) => prev + 1);
+  }
+
+  return (
+    <div>
+      <p>Shots Attempted: {AttemptedCount}</p>
+      <p>Shots Made: {MadeCount}</p>
+      <p>
+        Percentage:{" "}
+        {AttemptedCount === 0
+          ? 0
+          : Math.round((MadeCount / AttemptedCount) * 100)}
+        %
+      </p>
+      <Button onClick={handleMade} variant="success">
+        Made
+      </Button>
+      <Button onClick={handleMiss} variant="danger">
+        Miss
+      </Button>
+    </div>
+  );
 }
