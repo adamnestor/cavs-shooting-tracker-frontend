adde49752e6b4aaf9de1fc79be414bb9c71d96e2
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2025-11-15 18:11:57 -0500

    add player to test type and update result page with player name and filter

--- src/components/common/TestResultRow.tsx
+++ src/components/common/TestResultRow.tsx
@@ -8,11 +8,11 @@ export function TestResultRow({ test }: TestResultRowProps) {
   return (
     <div className="border rounded-lg p-4 flex justify-between items-center shadow-md hover:shadow-xl hover:border=[#fdbb30] transition-all duration-200 cursor-pointer">
       <div>
+        <p className=" text-xl font-semibold text-[#6d002d]">
+          {test.player.firstName} {test.player.lastName}
+        </p>
         <p className="font-semibold text-[#041e42]">
           {new Date(test.startTime).toLocaleDateString()}
-        </p>
-        <p className="font-semibold text-[#6d002d]">
-          {new Date(test.startTime).toLocaleTimeString()}
         </p>
       </div>
       <p className="text-2xl font-bold text-[#041e42]">{test.made}/100</p>
